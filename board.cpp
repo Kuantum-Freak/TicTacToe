1cddf97b28bb98e799b1d320492679e5941cace7
Aryan Gupta <kuantum.Freak@gmail.com>
2017-04-15 17:14:24 -0400

    Finished Moving docs To .h Files

--- board.cpp
+++ board.cpp
@@ -29,25 +29,14 @@ typedef std::vector<std::vector<char> > Board;
 
 Board board;
 
-/* 
-	@summary		Intantized the board, sets all the places to a space
-	
-	@param			VOID
-	@return			VOID
- */
+
 void initBoard() {
 	board.push_back(vector<char>(3, ' '));
 	board.push_back(vector<char>(3, ' '));
 	board.push_back(vector<char>(3, ' '));
 }
 
-/* 
-	@summary		Updates the board with a 'X' or 'O'
-	
-	@param			int: the place on the board (the numbers goes from the top 
-						 row: 0, 1, 2; then the next row:3, 4, 5; etc.)
-	@return			VOID
- */
+
 void updateBoard(int choice) {
 	if(userTurn) {
 		board[choice / 3][choice % 3] = playerPeices[P1PIECE];
@@ -59,25 +48,13 @@ void updateBoard(int choice) {
 	// outputBoards();
 }
 
-/* 
-	@summary		Checks if the choice is valid or not. Valid means the space
-					is empty. (No 'X' or 'O' char there)
-	
-	@param			int: choice to check
-	@return			bool: is it a valid choice or not
- */
+
 bool checkValidChoice(int choice) {
 	return  board[choice / 3][choice % 3] == 'X' ||
 			board[choice / 3][choice % 3] == 'O';
 }
 
-/* 
-	@summary		Checks to see if the board is filled up with 'X' or 'O'
-					or not
-	
-	@param			VOID
-	@return			bool: if the board is filled or not
- */
+
 bool boardFilled() {
 	int counter = 0;
 	for(auto &i: board)
@@ -87,12 +64,7 @@ bool boardFilled() {
 	return counter == 9;
 }
 
-/* 
-	@summary		Cleans up board so we can play again
-	
-	@param			VOID
-	@return			VOID
- */
+
 void cleanUpBoard() {
 	for(auto &i: board)
 		for (auto &j: i)
--- h/board.h
+++ h/board.h
@@ -22,14 +22,46 @@
 
 extern std::vector<std::vector<char> > board;
 
+/* 
+	@summary		Intantized the board, sets all the places to a space
+	
+	@param			VOID
+	@return			VOID
+ */
 void initBoard();
 
+/* 
+	@summary		Updates the board with a 'X' or 'O'
+	
+	@param			int: the place on the board (the numbers goes from the top 
+						 row: 0, 1, 2; then the next row:3, 4, 5; etc.)
+	@return			VOID
+ */
 void updateBoard(int choice);
 
+/* 
+	@summary		Checks if the choice is valid or not. Valid means the space
+					is empty. (No 'X' or 'O' char there)
+	
+	@param			int: choice to check
+	@return			bool: is it a valid choice or not
+ */
 bool checkValidChoice(int choice);
-
+/* 
+	@summary		Checks to see if the board is filled up with 'X' or 'O'
+					or not
+	
+	@param			VOID
+	@return			bool: if the board is filled or not
+ */
 bool boardFilled();
 
+/* 
+	@summary		Cleans up board so we can play again
+	
+	@param			VOID
+	@return			VOID
+ */
 void cleanUpBoard();
 
 #endif
