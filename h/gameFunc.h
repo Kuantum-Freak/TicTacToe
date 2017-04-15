68bc2c476027b12e7288397e03ab10a87ad5a5e6
Aryan Gupta <kuantum.Freak@gmail.com>
2017-04-15 17:39:28 -0400

    Change doc Comment Type

--- h/gameFunc.h
+++ h/gameFunc.h
@@ -18,37 +18,22 @@
 #ifndef GAME_FUNC_H_
 #define GAME_FUNC_H_
 
-/* 
-	@summary		Finds out if a player has won or not or the board is full
-	
-	@param			VOID
-	@return			bool | If a player has won or the board is full then true
- */
+
+/// @summary		Finds out if a player has won or not or the board is full
+/// @return			bool | If a player has won or the board is full then true
 bool playerWin();
 
-/* 
-	@summary		Changes the turn
-	
-	@param			VOID
-	@return			VOID
- */
+/// @summary		Changes the turn
 void changeTurn();
 
-/* 
-	@summary		Gets the Winner
-	
-	@param			VOID
-	@return			string: containing the winner
- */
+/// @summary		Gets the Winner
+/// @return			string: containing the winner
 std::string getWinner();
 
-/* 
-	@summary		checks if there is a '3 in a row' of a specified char, used 
+/// @summary		checks if there is a '3 in a row' of a specified char, used \
 					in various functions to find out the winner
-	
-	@param			char: to check if its 3 in a row
-	@return			bool: if char occurs 3 in a row
- */
+/// @param			char: to check if its 3 in a row
+/// @return			bool: if char occurs 3 in a row
 bool win(const char CHAR);
 
 #endif
--- h/graphics.h
+++ h/graphics.h
@@ -20,103 +20,49 @@
 
 #include <SDL.h>
 
-/* 
-	@summary		Initialize SDL, SDL_TTF, and SDL_Image. Then load the images
+/// @summary		Initialize SDL, SDL_TTF, and SDL_Image. Then load the images\
 					and fonts we need
-	
-	@param			VOID
-	@return			VOID
- */
 void initGraphics();
 
-/* 
-	@summary		Initialize SDL, SDL_TTF, and SDL_Image
-	
-	@param			VOID
-	@return			VOID
- */
+/// @summary		Initialize SDL, SDL_TTF, and SDL_Image
 void initSDL();
 
-/* 
-	@summary		Loads all the images we need. The X image and O image
-	
-	@param			VOID
-	@return			VOID
- */
+/// @summary		Loads all the images we need. The X image and O image
 void loadPiecesIMG();
 
-/* 
-	@summary		Loads a image and optamizes it for the current screen
-	
-	@param			string: path to image
-	@return			SDL_Surface*: The optomized SDL_Surface
- */
+/// @summary		Loads a image and optamizes it for the current screen
+/// @param			string: path to image
+/// @return			SDL_Surface*: The optomized SDL_Surface
 SDL_Texture* loadIMG(std::string path);
 
-/* 
-	@summary		Loads the font and sets the font sizes
-	
-	@param			VOID
-	@return			VOID
- */
+/// @summary		Loads the font and sets the font sizes
+
 void loadFont();
 
-/* 
-	@summary		Displays the current state of the board
-	
-	@param			VOID
-	@return			VOID
- */
+/// @summary		Displays the current state of the board
 void displayBoard();
 
 
-/* 
-	@summary		Asks the user to pick a char (X or O)
-	
-	@param			VOID
-	@return			char: the peice the user wants to be
- */
+/// @summary		Asks the user to pick a char (X or O)
+/// @return			char: the peice the user wants to be
 char askChar();
 
-/* 
-	@summary		Displays the legal stuff on the opening render
-	
-	@param			VOID
-	@return			VOID
- */
+/// @summary		Displays the legal stuff on the opening render
 void displayLegal();
 
-/* 
-	@summary		Displays the winner and asks if we should play again or exit
-	
-	@param			string: winner of the game
-	@return			bool: if we should play again or not
- */
+/// @summary		Displays the winner and asks if we should play again or exit
+/// @param			string: winner of the game
+/// @return			bool: if we should play again or not
 bool displayWinner(std::string winner);
 
-/* 
-	@summary		Gets Where the user wants to place his piece
-	
-	@param			VOID
-	@return			int: the place where he wants to place his piece
- */
+/// @summary		Gets Where the user wants to place his piece
+/// @return			int: the place where he wants to place his piece
 int getUserChoice();
 
-/* 
-	@summary		Cleans up the graphics for the next run. Currently does
-					nothing
-	
-	@param			VOID
-	@return			VOID
- */
+/// @summary		Cleans up the graphics for the next run. Currently does nothing
 void cleanUpGraphics();
 
-/* 
-	@summary		Deallocates all the surfaces and memory
-	
-	@param			VOID
-	@return			VOID
- */
+/// @summary		Deallocates all the surfaces and memory
 void destroy();
 
 #endif
